f4eb3ef9bff38bb00056c593713642ca85e761fe
Patrick Stearns <pdeans1986@gmail.com>
2023-06-05 08:55:06 -0700

    Added property and method type hints, code clean up

--- src/Auth.php
+++ src/Auth.php
@@ -10,131 +10,111 @@ use pdeans\Miva\Api\Exceptions\InvalidValueException;
 final class Auth
 {
     /**
-     * API access token
+     * API access token.
      *
      * @var string
      */
-    private $access_token;
+    private string $accessToken;
 
     /**
-     * API authenication header name
+     * API authentication header name.
      *
      * @var string
      */
-    private $auth_header_name;
+    private string $authHeaderName;
 
     /**
-     * List of valid HMAC types
+     * List of valid HMAC types.
      *
      * @var array
      */
-    private $hmac_list;
+    private array $hmacList;
 
     /**
-     * API request HMAC signature
+     * API request HMAC signature.
      *
      * @var string
      */
-    private $hmac_signature;
+    private string $hmacSignature;
 
     /**
-     * The HMAC type
+     * The HMAC type.
      *
      * @var string
      */
-    private $hmac_type;
+    private string $hmacType;
 
     /**
-     * API private key
+     * API private key.
      *
      * @var string
      */
-    private $private_key;
+    private string $privateKey;
 
     /**
-     * Construct Auth object
-     *
-     * @param string $access_token The API access token
-     * @param string $private_key  The API private key
-     * @param string $hmac_type    The HMAC type
+     * Create a new API auth instance.
      */
-    public function __construct(string $access_token, string $private_key, string $hmac_type = 'sha256')
+    public function __construct(string $accessToken, string $privateKey, string $hmacType = 'sha256')
     {
-        $this->access_token = $access_token;
-        $this->private_key  = $private_key;
+        $this->accessToken = $accessToken;
+        $this->privateKey = $privateKey;
 
         $this->setHmacList();
-        $this->setHmacType($hmac_type);
+        $this->setHmacType($hmacType);
 
-        $this->auth_header_name = 'X-Miva-API-Authorization';
-        $this->hmac_signature  = null;
+        $this->authHeaderName = 'X-Miva-API-Authorization';
+        $this->hmacSignature = null;
     }
 
     /**
-     * Create an API authorization header
-     *
-     * @param string $data  The API request body
-     *
-     * @return string
+     * Create an API authorization header.
      */
-    public function createAuthHeader(string $data)
+    public function createAuthHeader(string $data): string
     {
-        return sprintf('%s: %s', $this->auth_header_name, $this->createAuthHeaderValue($data));
+        return sprintf('%s: %s', $this->authHeaderName, $this->createAuthHeaderValue($data));
     }
 
     /**
-     * Create an API authorization header value
-     *
-     * @param string $data  The API request body
-     *
-     * @return string
+     * Create an API authorization header value.
      */
-    public function createAuthHeaderValue(string $data)
+    public function createAuthHeaderValue(string $data): string
     {
-        if ((string)$this->hmac_type === '') {
-            return sprintf('MIVA %s', $this->access_token);
+        if ((string) $this->hmacType === '') {
+            return sprintf('MIVA %s', $this->accessToken);
         }
 
-        $this->hmac_signature = $this->createHmacSignature($data);
+        $this->hmacSignature = $this->createHmacSignature($data);
 
         return sprintf(
             'MIVA-HMAC-%s %s:%s',
-            strtoupper($this->hmac_type),
-            $this->access_token,
-            base64_encode($this->hmac_signature)
+            strtoupper($this->hmacType),
+            $this->accessToken,
+            base64_encode($this->hmacSignature)
         );
     }
 
     /**
-     * Generate a keyed hash value using the HMAC type
-     *
-     * @param string $data  The message to be hashed
-     *
-     * @return string
+     * Generate a keyed hash value using the HMAC type.
      */
-    protected function createHmacSignature(string $data)
+    protected function createHmacSignature(string $data): string
     {
-        return hash_hmac($this->hmac_type, $data, base64_decode($this->private_key), true);
+        return hash_hmac($this->hmacType, $data, base64_decode($this->privateKey), true);
     }
 
     /**
-     * Get the API authorization header
-     *
-     * @param string $data  The API request body
-     *
-     * @return array
+     * Get the API authorization header.
      */
-    public function getAuthHeader(string $data)
+    public function getAuthHeader(string $data): array
     {
-        return [$this->auth_header_name => $this->createAuthHeaderValue($data)];
+        return [$this->authHeaderName => $this->createAuthHeaderValue($data)];
     }
 
     /**
      * Set the list of valid HMAC types
      */
-    protected function setHmacList()
+    protected function setHmacList(): self
     {
-        $this->hmac_list = [
+        $this->hmacList = [
             'sha1',
             'sha256',
         ];
@@ -143,32 +123,30 @@ final class Auth
     }
 
     /**
-     * Set the HMAC type
-     *
-     * @param string $hmac_type
+     * Set the HMAC type.
      */
-    protected function setHmacType(string $hmac_type)
+    protected function setHmacType(string $hmacType): self
     {
-        if (!$this->hmac_list) {
+        if (!$this->hmacList) {
             $this->setHmacList();
         }
 
-        if ($hmac_type === '' || (is_string($this->private_key) && $this->private_key === '')) {
-            $this->hmac_type = '';
+        if ($hmacType === '' || (is_string($this->privateKey) && $this->privateKey === '')) {
+            $this->hmacType = '';
         } else {
-            $hmac_type_formatted = strtolower($hmac_type);
+            $hmacTypeFormatted = strtolower($hmacType);
 
-            if (!in_array($hmac_type_formatted, $this->hmac_list)) {
+            if (!in_array($hmacTypeFormatted, $this->hmacList)) {
                 throw new InvalidValueException(
                     sprintf(
                         'Invalid HMAC type "%s" provided. Valid HMAC types: "%s".',
-                        $hmac_type,
-                        implode('", "', $this->hmac_list)
+                        $hmacType,
+                        implode('", "', $this->hmacList)
                     )
                 );
             }
 
-            $this->hmac_type = $hmac_type_formatted;
+            $this->hmacType = $hmacTypeFormatted;
         }
 
         return $this;
